3d91936df48a8698facfe8225d176a552349bd3a
Karthik <https://karthikpillalamarri@bitbucket.org/t4sg/namr-ffg-2020-drug-free-clubs-of-america.git>
2021-04-11 19:58:41 -0400

    trying on-screen keyboard.

--- CustomizableWordpad/App.js
+++ CustomizableWordpad/App.js
@@ -1,32 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import React, { useState } from 'react';
+// import { Keyboard, StyleSheet, Text, View, TextInput } from 'react-native';
 
 
-const App = () => {
-  const [text, setText] = useState('');
+// const App = () => {
+//   const [text, setText] = useState('');
+//   return (
+//     <View style={{padding: 10, alignContent: 'center'}}>
+//       <TextInput
+//         style={{height: 700, width: 700, fontSize: 42, alignContent: 'center'}}
+//         placeholder="Start Typing!"
+//         onChangeText={text => setText(text)}
+//         defaultValue={text}
+//       />
+//       <Text style={{padding: 10, fontSize: 42}}>
+//         {/* {text.split(' ').map((word) => word && '🍕').join(' ')} */}
+//       </Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+// export default App;
+
+import React, { useState, useEffect } from "react";
+import { Keyboard, Text, TextInput, StyleSheet, View } from "react-native";
+
+const Example = () => {
+  useEffect(() => {
+    Keyboard.addListener("keyboardDidShow", _keyboardDidShow);
+    Keyboard.addListener("keyboardDidHide", _keyboardDidHide);
+
+    // cleanup function
+    return () => {
+      Keyboard.removeListener("keyboardDidShow", _keyboardDidShow);
+      Keyboard.removeListener("keyboardDidHide", _keyboardDidHide);
+    };
+  }, []);
+
+  const [keyboardStatus, setKeyboardStatus] = useState(undefined);
+  const _keyboardDidShow = () => setKeyboardStatus("Keyboard Shown");
+  const _keyboardDidHide = () => setKeyboardStatus("Keyboard Hidden");
+
   return (
-    <View style={{padding: 10, alignContent: 'center'}}>
+    <View style={style.container}>
       <TextInput
-        style={{height: 700, width: 700, fontSize: 42, alignContent: 'center'}}
-        placeholder="Start Typing!"
-        onChangeText={text => setText(text)}
-        defaultValue={text}
+        style={style.input}
+        placeholder='Click here…'
+        onSubmitEditing={Keyboard.dismiss}
       />
-      <Text style={{padding: 10, fontSize: 42}}>
-        {/* {text.split(' ').map((word) => word && '🍕').join(' ')} */}
-      </Text>
+      <Text style={style.status}>{keyboardStatus}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 36
   },
+  input: {
+    padding: 10,
+    borderWidth: 0.5,
+    borderRadius: 4
+  },
+  status: {
+    padding: 10,
+    textAlign: "center"
+  }
 });
 
-export default App;
+export default Example;
