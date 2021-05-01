76a4f3573e953b0faa0c80ee53e21c83d4dd1923
Karthik <https://karthikpillalamarri@bitbucket.org/t4sg/namr-ffg-2020-drug-free-clubs-of-america.git>
2021-05-01 19:19:13 -0400

    made wordpad responsive.

---
+++ CustomizableWordpad/screens/TabOneScreen.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import { TextInput } from 'react-native';
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import {
+  widthPercentageToDP as wp2dp,
+  heightPercentageToDP as hp2dp,
+} from 'react-native-responsive-screen';
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Customizable Wordpad</Text>
+      <TextInput style={styles.wordpad}></TextInput>
+      {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  wordpad: {
+    width: '80%',
+    height: '80%',
+    margin: 12,
+    borderWidth: 1,
+    color: 'black',
+    backgroundColor: 'white',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
