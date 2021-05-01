52ad252d94bb5e8aec5b4039db7a31a2a3d75e04
Karthik <https://karthikpillalamarri@bitbucket.org/t4sg/namr-ffg-2020-drug-free-clubs-of-america.git>
2021-05-01 19:23:24 -0400

    input text appears at top left.

--- CustomizableWordpad/screens/TabOneScreen.tsx
+++ CustomizableWordpad/screens/TabOneScreen.tsx
@@ -12,7 +12,7 @@ export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Customizable Wordpad</Text>
-      <TextInput style={styles.wordpad}></TextInput>
+      <TextInput style={styles.wordpad} multiline={true}></TextInput>
       {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
     </View>
